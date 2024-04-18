fb375c33f02388f3ec06e68b74d2352e441a71e2
danilins133 <129066504+danilins133@users.noreply.github.com>
2024-04-18 17:20:05 -0300

    Add files via upload

---
+++ Untitled6.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+	char nome[50];
+	char curso[50];
+	char telefone[20];
+	float nota1, nota2;
+} Dados;
+
+void leitura_arquivo(){
+	
+	FILE *arquivo;
+	FILE *arquivo2;
+	
+	float media = 0;
+	
+	arquivo = fopen("DadosEntrada.csv", "r");
+	arquivo2 = fopen("SituacaoFinal.csv", "w");
+	
+	Dados info;
+	
+	if (arquivo == NULL) {
+		printf("Erro ao abrir o arquivo.\n");
+		exit(1);
+	} 
+	
+	fscanf(arquivo, "%*[^\n]\n");
+	
+	while(fscanf(arquivo, "%49[^,],%19[^,],%49[^,],%f,%f", info.nome, info.telefone, info.curso, &info.nota1, &info.nota2) != EOF){	
+	
+	media = (info.nota1 + info.nota2) / 2;
+	
+	fprintf(arquivo2, "Nome: %s\n", info.nome);
+	fprintf(arquivo2, "Media: %.2f\n", media);
+	
+	if (media >= 7.0){
+	
+    	fprintf(arquivo2, "APROVADO\n");
+    	fprintf(arquivo2, "\n");
+    	
+	}
+	else if (media < 7.0){
+    	fprintf(arquivo2, "REPROVADO\n");	
+    	fprintf(arquivo2, "\n");
+
+	}
+	
+	fscanf(arquivo, "%*c");
+	
+	}
+	
+	fclose(arquivo);
+	fclose(arquivo2);
+	
+};
+
+int main(){
+	
+	leitura_arquivo();
+
+	return 0;
+
+};
